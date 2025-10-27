4455e7c5f85be28221b7c9ee0fa3ac40fa3f98eb
Felipe Macedo <felipe.x.macedo@gmail.com>
2025-10-26 23:15:12 -0300

    Change how to load documents

--- src/main/java/br/com/xmacedo/examstudylibraryguideai/config/DocumentConfig.java
+++ src/main/java/br/com/xmacedo/examstudylibraryguideai/config/DocumentConfig.java
@@ -1,7 +1,11 @@
 package br.com.xmacedo.examstudylibraryguideai.config;
 
+import br.com.xmacedo.examstudylibraryguideai.service.ExamService;
+import br.com.xmacedo.examstudylibraryguideai.service.TopicService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import java.io.IOException;
 
@@ -9,9 +13,21 @@ import java.io.IOException;
 public class DocumentConfig {
 
     private final VectorStore vectorStore;
+    private final ExamService examService;
+    private final TopicService topicService;
+    private final ObjectMapper mapper;
 
-    public DocumentConfig(VectorStore vectorStore) {
+    //    @Value("${app.documents.patterns:*.md}")
+    private static final String PATH_FOR_EXAMS = "/data/exam.json";
+
+    @Value("${app.documents.path:../}")
+    private static String PATH_FOR_DOCUMENTS;
+
+    public DocumentConfig(VectorStore vectorStore, ExamService examService, TopicService topicService, ObjectMapper mapper) {
         this.vectorStore = vectorStore;
+        this.examService = examService;
+        this.topicService = topicService;
+        this.mapper = mapper;
     }
 
     @PostConstruct
